ba9b4c70aaa0a7683c09ddabd265b76c664c8425
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-07-13 21:54:08 +0530

    03 out 35

---
+++ 03 out of 35 Understand the react flow and structure/vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+import Chai from './chai.jsx'
+
+export const App = () => {
+  return (
+    <>
+      <h1>SABED</h1>
+      <Chai />
+    </>
+  )
+}
---
+++ 03 out of 35 Understand the react flow and structure/vite-project/src/Chai.jsx
@@ -0,0 +1,5 @@
+export default function Chai() {
+  return (
+    <div>chai</div>
+  )
+}
