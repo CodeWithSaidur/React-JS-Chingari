b2f7785cf8e927fb9b5e9cf26fd55eee7417c0d6
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-07-15 21:01:49 +0530

    07 out of 35

---
+++ 07 out of 35 Tailwind and Props in reactjs/src/App.jsx
@@ -0,0 +1,10 @@
+import { Card } from './components/Card.jsx'
+
+export const App = () => {
+  return (
+    <>
+      <Card AlianName={10} AlClan="ALC 012 " />
+      <Card AlianName={10} />
+    </>
+  )
+}
