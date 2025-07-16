5d058775ced0bae67aa81a8bfabae1c4d6cad687
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-07-17 01:30:34 +0530

    10 out of 38

--- 10 out of 38 useEffect useRef and useCallback with 1 project/src/App.jsx
+++ 10 out of 38 useEffect useRef and useCallback with 1 project/src/App.jsx
@@ -1,20 +1,51 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export const App = () => {
   const [length, setLength] = useState(8)
   const [includeNumbers, setIncludeNumbers] = useState(true)
   const [includeCharrecter, setincludeCharrecter] = useState(true)
+  const [token, setToken] = useState('')
 
   // console.log('Length', length)
   // console.log('Numbers', includeNumbers)
   // console.log('Charrecters', includeCharrecter)
   // console.log('_____________________________')
 
+  // generate token
+  const generateToken = useCallback(() => {
+    let ListOfCharrecter =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+    if (includeNumbers) {
+      ListOfCharrecter += '1234567890'
+    }
+
+    if (includeCharrecter) {
+      ListOfCharrecter += '!@#$%^&*(*)_+-='
+    }
+
+    let generatedToken = ''
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(
+        Math.random() * ListOfCharrecter.length
+      )
+      generatedToken += ListOfCharrecter.charAt(randomIndex)
+    }
+
+    setToken(generatedToken)
+  }, [length, includeNumbers, includeCharrecter])
+
+  useEffect(() => {
+    generateToken()
+  }, [generateToken])
+
   return (
     <div>
       <h1>Password Generaror</h1>
       <input
         type="text"
+        value={token}
         style={{ border: '2px solid red', borderRadius: '10px' }}
       />{' '}
       <br />
