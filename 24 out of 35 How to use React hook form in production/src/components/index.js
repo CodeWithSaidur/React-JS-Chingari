2b639eac201233bcd49d1dfff5cca4f504e10db4
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-08-05 15:47:57 +0530

    24 out of 35

---
+++ 24 out of 35 How to use React hook form in production/src/components/index.js
@@ -0,0 +1,9 @@
+import Header from './Header/Header.jsx'
+import Footer from './Footer/Footer.jsx'
+import Container from './container/Container.jsx'
+import Logo from './Logo.jsx'
+import LogoutBtn from './Header/LogoutBtn.jsx'
+import Button from './Button.jsx'
+import Input from './Input.jsx'
+
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input }
