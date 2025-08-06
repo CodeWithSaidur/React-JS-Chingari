d3945615fcdc99d70ac0fb2068e5374c95e6772f
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-08-07 00:36:02 +0530

    26 out of 35

---
+++ 26 out of 35 Building pages  chai aur react/src/components/index.js
@@ -0,0 +1,32 @@
+import Header from './Header/Header.jsx'
+import Footer from './Footer/Footer.jsx'
+import Container from './container/Container.jsx'
+import Logo from './Logo.jsx'
+import LogoutBtn from './Header/LogoutBtn.jsx'
+import Button from './Button.jsx'
+import Input from './Input.jsx'
+import RTE from './RTE.jsx'
+import Select from './Select.jsx'
+import Signup from './Signup.jsx'
+import Login from './Login.jsx'
+import Post from './PostCard.jsx'
+import PostForm from './post-form/PostForm.jsx'
+import PostCard from './PostCard.jsx'
+import AuthLayout from './AuthLayout.jsx'
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Input,
+  RTE,
+  Select,
+  Signup,
+  Login,
+  Post,
+  PostForm,
+  PostCard,
+  AuthLayout
+}
